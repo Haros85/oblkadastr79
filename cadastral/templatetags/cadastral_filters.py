634536dce83605c0426892a0f6e6dcf93807979c
Haros85 <ruslan@antonenko.me>
2021-03-18 23:30:43 +1000

    add cadastral_filters

---
+++ cadastral/templatetags/cadastral_filters.py
@@ -0,0 +1,19 @@
+from django import template
+from cadastral.models import Act
+
+register = template.Library()
+
+
+@register.filter(name='count_objects')
+def get_filter_values(value):
+    count_objects = value
+    remainder = count_objects % 10
+    if count_objects >= 10 and count_objects <= 20:
+        phrase = f'Ещё {str(count_objects)} объектов'
+    elif remainder == 1:
+        phrase = f'Ещё {str(count_objects)} объект'
+    elif remainder >= 2 and remainder <= 4:
+        phrase = f'Ещё {str(count_objects)} объекта'
+    elif remainder >= 5 or remainder <= 9:
+        phrase = f'Ещё {str(count_objects)} объектов'
+    return phrase
